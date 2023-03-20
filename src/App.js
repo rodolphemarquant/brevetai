6300d1e1fcc0bca2b93b801186424b79e49396bd
Rodolphe Marquant <rodolphe.marquant@gmail.com>
2023-03-20 13:36:36 +0100

    Removed reveal.js dependency

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 //import "./styles.css";
 import "./styles.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
-import "/node_modules/reveal.js/dist/reveal.css";
-import "/node_modules/reveal.js/dist/theme/black.css";
 import Home from "./Home";
 
 export default function App() {
