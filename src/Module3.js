eb57af975f6f3dfb686e27c91835c6139171a6f2
rodolphemarquant <rodolphe.marquant@gmail.com>
2023-03-21 14:59:55 +0000

    Updated gitignore to remove builds/ added images updated Module3

--- src/Module3.js
+++ src/Module3.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 import StyledButton from "./components/utils/StyledButton";
 import Spinner from "./components/utils/Spinner";
 import Model from "./components/Module3/Model";
+import flowers34 from "./public/Module3/BRV_AI_Flowers-34.png";
+import flowers35 from "./public/Module3/BRV_AI_Flowers-35.png";
+import flowers36 from "./public/Module3/BRV_AI_Flowers-36.png";
+import flowers37 from "./public/Module3/BRV_AI_Flowers-37.png";
 
 const Module3Div = styled.div`
   display: flex;
@@ -41,6 +45,12 @@ const Image = styled.img`
   width: 100%;
 `;
 
+const ModelFound = styled.img`
+  width: 150px;
+  -webkit-filter: drop-shadow(2px 1px 0 black) drop-shadow(-1px -1px 0 black);
+  filter: drop-shadow(2px 1px 0 black) drop-shadow(-1px -1px 0 black);
+`;
+
 function Module3() {
   const [displaySpinner, setDisplaySpinner] = useState(false);
 
@@ -54,19 +64,24 @@ function Module3() {
     <Module3Div>
       <LeftDiv>
         <Image src={"https://placehold.co/600x600"} alt={"Template"} />
+        <ModelFound
+          src={flowers37}
+          alt={"image"}
+          style={{ position: "absolute" }}
+        />
       </LeftDiv>
       <RightDiv>
-        <Model src={"https://placehold.co/200x200"} alt={"image"} active />
-        <Model src={"https://placehold.co/200x200"} alt={"image"} />
-        <Model src={"https://placehold.co/200x200"} alt={"image"} />
+        <Model src={flowers34} alt={"image"} />
+        <Model src={flowers35} alt={"image"} />
+        <Model src={flowers36} alt={"image"} />
 
-        <Model src={"https://placehold.co/200x200"} alt={"image"} />
-        <Model src={"https://placehold.co/200x200"} alt={"image"} />
-        <Model src={"https://placehold.co/200x200"} alt={"image"} />
+        <Model src={flowers37} alt={"image"} />
+        <Model src={flowers34} alt={"image"} />
+        <Model src={flowers35} alt={"image"} />
 
-        <Model src={"https://placehold.co/200x200"} alt={"image"} />
-        <Model src={"https://placehold.co/200x200"} alt={"image"} />
-        <Model src={"https://placehold.co/200x200"} alt={"image"} />
+        <Model src={flowers36} alt={"image"} />
+        <Model src={flowers37} alt={"image"} />
+        <Model src={flowers34} alt={"image"} />
 
         <StyledButton onClick={validate} style={{ marginTop: "30px" }}>
           Valider
--- src/components/Module3/Model.js
+++ src/components/Module3/Model.js
@@ -5,6 +5,8 @@ const ImageModel = styled.img`
   cursor: pointer;
   max-width: 26%;
   height: auto;
+  aspect-ratio: 1 / 1;
+  padding: 8px;
   ${(props) =>
     props.isSelected
       ? "box-shadow: 0px 0px 9px 1px rgba(255, 142, 123, 0.68)"
